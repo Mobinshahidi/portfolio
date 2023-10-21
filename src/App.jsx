37230781fd711448a3613fac6742cbd9abb89a7d
Mobinshahidi <mowbinsh@gmail.com>
2023-10-22 01:13:46 +0330

    app change again

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				viewBox="0 0 1440 320"
-				style={{ margin: '0', padding: '0', display: 'block', height: '50vh' }}
+				style={{ margin: '0', padding: '0', display: 'block' }}
 			>
 				<path
 					fill="#3D3B3B"
@@ -46,7 +46,7 @@ function App() {
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				viewBox="0 0 1440 320"
-				style={{ margin: '0', padding: '0', display: 'block', height: '50vh' }}
+				style={{ margin: '0', padding: '0', display: 'block' }}
 			>
 				<path
 					fill="#3D3B3B"
